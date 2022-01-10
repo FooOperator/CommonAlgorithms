cfda6a3b3eda0397c0b4688df8acb08501dac40c
devgcard@gmail.com <devgcard@gmail.com>
2022-01-10 02:23:43 +0000

    Add project files.

---
+++ BinarySearch.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommonAlgorithms
+{
+    public class BinarySearch
+    {
+        public static int IndexOf(int[] collection, int target)
+        {
+            int low = 0, high = collection.Length - 1;
+
+            while (low <= high)
+            {
+                int mid = (low + high) / 2;
+
+                if (target == collection[mid]) return mid;
+
+                else if (target < collection[mid])
+                {
+                    high = mid - 1;
+                }
+
+                else
+                {
+                    low = mid + 1;
+                }
+            }
+
+            // target doesn't exist in the array
+            return -1;
+        }
+    }
+}
---
+++ Fibonacci.cs
@@ -0,0 +1,20 @@
+﻿/**/// See https://aka.ms/new-console-template for more information
+public class Fibonacci
+{
+    public static uint GetNthFibonacci(uint nth)
+    {
+        return nth <= 1 ? 1 : GetNthFibonacci(nth - 1) + GetNthFibonacci(nth - 2);
+    }
+
+    public static uint[] GetFibonacciUntil(uint end)
+    {
+        uint[] fibs = new uint[end + 1];
+
+        for (uint i = 0; i < end + 1; i++)
+        {
+            fibs[i] = GetNthFibonacci(i);
+        }
+
+        return fibs;
+    }
+}
